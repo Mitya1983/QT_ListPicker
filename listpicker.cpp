526df8b998897762d27826fdb262020544be3e63
Mitya1983 <tristan.it@outlook.com>
2019-01-04 01:40:28 +0200

    setShownValues implemented taking into account cirlced/nonsirclied view

--- listpicker.cpp
+++ listpicker.cpp
@@ -3,34 +3,40 @@
 #include <QLayout>
 
 
-ListPicker::ListPicker(QWidget *parent, const int &numberOfRowsShown) :
+ListPicker::ListPicker(const int &numberOfRowsShown, QWidget *parent) :
     QWidget(parent),
     layout(new QVBoxLayout(this)),
     labels(QVector<QLabel*>(numberOfRowsShown)),
     selectedLabel(numberOfRowsShown / 2)
 {
+    circleItems = true;
     presentationSetup();
-    createNumberList(5, 1);
+    createNumberList(100, 1);
+    setShownValues();
 }
 
-ListPicker::ListPicker(const int &numberOfListElements, QWidget *parent, const int &numberOfRowsShown) :
+ListPicker::ListPicker(const int &numberOfListElements, const int &numberOfRowsShown, QWidget *parent) :
     QWidget (parent),
     layout(new QVBoxLayout(this)),
     labels(QVector<QLabel*>(numberOfRowsShown)),
     selectedLabel(numberOfRowsShown / 2)
 {
+    circleItems = true;
     presentationSetup();
     list = QVector<QString>(numberOfListElements);
+    maxShownItems = list.size();
 }
 
-ListPicker::ListPicker(std::initializer_list<QString> _list, QWidget *parent, const int &numberOfRowsShown) :
+ListPicker::ListPicker(std::initializer_list<QString> _list, const int &numberOfRowsShown, QWidget *parent) :
     QWidget (parent),
     layout(new QVBoxLayout(this)),
     labels (QVector<QLabel*>(numberOfRowsShown)),
     selectedLabel(numberOfRowsShown / 2)
 {
+    circleItems = true;
     presentationSetup();
     list = QVector<QString>(_list);
+    maxShownItems = list.size();
 }
 
 void ListPicker::createNumberList(const int &numberOfListElements, int startValue)
@@ -38,20 +44,19 @@ void ListPicker::createNumberList(const int &numberOfListElements, int startValu
     if (list.size() != numberOfListElements)
     {
         list.resize(numberOfListElements);
-        qDebug() << "list resized";
     }
     for (auto i = 0; i < numberOfListElements; i++)
     {
         list[i] = QString::number(startValue++);
-        qDebug() << list.at(i);
     }
-    qDebug() << list.at(0);
+    maxShownItems = numberOfListElements;
 }
 
-ListPicker::~ListPicker()
+QString ListPicker::selectedValue()
 {
-    std::for_each(labels.begin(), labels.end(), [] (QLabel *p) {delete p;});
+    return labels[selectedLabel]->text();
 }
+
 
 void ListPicker::presentationSetup()
 {
@@ -61,15 +66,95 @@ void ListPicker::presentationSetup()
     for (int i = 0, k = labels.size(); i < k; i ++)
     {
         labels[i] = new QLabel(this);
-        labels[i]->setText("TEST");
         labels[i]->setAlignment(Qt::AlignCenter);
         QFont font = labels[i]->font();
         font.setBold(true);
         i == selectedLabel ? labels[i]->setEnabled(true) : labels[i]->setEnabled(false);
         i <= selectedLabel ? font.setPointSize(labels[i]->height() - (fontDecrement + (fontDecrement * (selectedLabel - i)))) :
-                            font.setPointSize(labels[i]->height() - (fontDecrement + (fontDecrement * (i - selectedLabel))));
+                             font.setPointSize(labels[i]->height() - (fontDecrement + (fontDecrement * (i - selectedLabel))));
         labels[i]->setFont(font);
         layout->addWidget(labels[i]);
     }
     this->setLayout(layout);
+}
+
+void ListPicker::setCircling(bool val)
+{
+    circleItems = val;
+}
+
+int ListPicker::previousIndex(int _currentIndex)
+{
+    if (_currentIndex > 0)
+        --_currentIndex;
+    else
+        _currentIndex = maxShownItems - 1;
+    return _currentIndex;
+}
+
+int ListPicker::nextIndex(int _currentIndex)
+{
+    if (_currentIndex < maxShownItems - 1)
+        ++_currentIndex;
+    else
+        _currentIndex = 0;
+    return _currentIndex;
+}
+
+void ListPicker::setShownValues(int _currentValueIndex)
+{
+    if (circleItems)
+    {
+        labels[selectedLabel]->setText(list[_currentValueIndex]);
+        int pr_index = previousIndex(_currentValueIndex);
+        for (int i = selectedLabel - 1; i >= 0; i--)
+        {
+            labels[i]->setText(list[pr_index]);
+            pr_index--;
+        }
+        int nt_index = nextIndex(_currentValueIndex);
+        for (int i = selectedLabel + 1, k = labels.size(); i < k; i++)
+        {
+            labels[i]->setText(list[nt_index]);
+            nt_index++;
+        }
+    }
+    else
+    {
+        labels[selectedLabel]->setText(list[_currentValueIndex]);
+        int pr_index = 0, nt_index = 0;
+        if (_currentValueIndex >= 0)
+            pr_index = _currentValueIndex - 1;
+        for (int i = selectedLabel - 1; i >= 0; i--)
+        {
+            if (pr_index >= 0)
+            {
+                labels[i]->setText(list[pr_index]);
+                pr_index--;
+            }
+            else
+                labels[i]->setText("");
+        }
+        if (_currentValueIndex < list.size())
+            nt_index = _currentValueIndex + 1;
+        for (int i = selectedLabel + 1, k = labels.size(); i < k; i++)
+        {
+            if (nt_index < list.size())
+            {
+                labels[i]->setText(list[nt_index]);
+                nt_index++;
+            }
+            else
+                labels[i]->setText("");
+        }
+    }
+}
+
+
+
+
+
+ListPicker::~ListPicker()
+{
+    std::for_each(labels.begin(), labels.end(), [] (QLabel *p) {delete p;});
 }
--- listpicker.h
+++ listpicker.h
@@ -8,9 +8,7 @@
 #include <QFont>
 #include <QVBoxLayout>
 //Implement list fillment
-//Implement setSownValues function
-//Reimplement maxDays on base of not to change the vector sizes but to limit the circle or shown rows.
-//That is vector is always created with 31 days but shown days are limited.
+
 
 class ListPicker : public QWidget
 {
@@ -18,19 +16,19 @@ class ListPicker : public QWidget
 
 public:
 //    ListPicker(QWidget *parent = nullptr);
-    ListPicker(QWidget *parent = nullptr,
-               const int &numberOfRowsShown = 5); //Number of shown items in th widget one selected item in the middle and -+items (2 by default)
+    ListPicker(const int &numberOfRowsShown = 5,
+               QWidget *parent = nullptr); //Number of shown items in th widget one selected item in the middle and -+items (2 by default)
     ListPicker(const int &numberOfListElements, //Size of a list
-               QWidget *parent = nullptr,
-               const int &numberOfRowsShown = 5);
+               const int &numberOfRowsShown = 5,
+               QWidget *parent = nullptr);
     ListPicker(std::initializer_list<QString> _list, //Creates list from string values passed to list
-               QWidget *parent = nullptr,
-               const int &numberOfRowsShown = 5);
+               const int &numberOfRowsShown = 5,
+               QWidget *parent = nullptr);
 
     void createNumberList(const int &numberOfListElements, int startValue); //Creates custom number list starting from startValue
     void createList(std::initializer_list<QString> list);
-    void createStringList(const QVector<QString> &_vector); //Creates custom string list from existing vector
-    void createStringList(const QList<QString> &_list); //Creates custom string list from existing list
+    void createStringList(QVector<QString> &&_vector); //Creates custom string list from existing vector
+    void createStringList(QList<QString> &&_list); //Creates custom string list from existing list
 
     QString selectedValue(); //Getting value of the selected item
     int selectedIndex(); //Getting index of the selected item
@@ -44,7 +42,7 @@ private:
 
     //Should list be circled
     bool circleItems;
-    void setCircling(bool _val); //Setting circling
+    void setCircling(bool val); //Setting circling
     bool isCircled(); //Checking if circled
 
     //Items storage
@@ -68,14 +66,14 @@ private:
 private slots:
 
     //Sets shown rows
-    //void setShownValues(int _currentValueIndex);
+    void setShownValues(int _currentValueIndex = 0);
 
 signals:
 
     //Used to invoke createDays each time when maxDay is changed (changing 28, 29, 30 and 31 days count)
 //    void onMaxShownItemsChanged(int maxShownItems);
 
-    //Used to invoke setDayesShown
+    //Used to invoke setShownValues
 //    void onSelectedValueChanged(int newSelectedValueIndex);
 
 public slots:
